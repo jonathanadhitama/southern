7ed64e61587ea1130e9c2d8c6392aa883a542364
jonathanadhitama <jonathanadhitama@users.noreply.github.com>
2020-10-23 09:46:55 +1100

    Initial commit

---
+++ resources/views/react.blade.php
@@ -0,0 +1,10 @@
+<html>
+    <title>
+        REACT IN LARAVEL
+    </title>
+    <body>
+        <div class="content" id="example">
+        </div>
+    </body>
+    <script type="text/javascript" src="js/app.js"></script>
+</html>
